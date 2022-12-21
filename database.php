afcacf0953c267c2a73d5f3ad2979899dcd948b4
HaikalAzka19 <116627949+HaikalAzka19@users.noreply.github.com>
2022-12-21 11:16:26 +0700

    updte

--- database.php
+++ database.php
@@ -1,15 +1,7 @@
-<?php
-$host = 'localhost';
-$db = 'id20043684_db';
-$usr = 'id20043684_user';
-$pwd = 'Projectteam12#';
-
-// parameter = hostname, username, password, database
-$conn = mysqli_connect($host, $usr, $pwd, $db); //true|false
+<?php 
+$conn = mysqli_connect("localhost", "root", "", "perpus");
 
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+  die("Gagal terhubung dengan database: " . mysqli_connect_error());
 }
-echo "Connected successfully";
-mysqli_close($conn);
 ?>
